782e62c29c30290ec4fc9a0eeb3a00304a465015
gojoizgod <142491463+gojoizgod@users.noreply.github.com>
2023-10-08 18:34:49 +1100

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,8 @@
+var vg_1 = "map2.json";
+vegaEmbed("#map_chart", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var vg_2 = "areachart.json";
+vegaEmbed("#area_chart", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
